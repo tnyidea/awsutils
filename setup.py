1e731b39da218f5ff464b24fbe5fde282d1d9424
Greg Behrend <15693980+gbnyc26@users.noreply.github.com>
2024-05-15 20:22:14 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
-from setuptools import setup, find_packages
-
-setup(
+setuptools.setup(
     name='awsutils',
-    version='0.0.3',
-    packages=find_packages('./python'),
+    version='0.0.4',
+    packages=packages=['./python/*'],
 )
